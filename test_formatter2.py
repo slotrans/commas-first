95da89d8e6b5cf1f91893c7efe50ff239ecac603
Noah Yetter <nyetter@gmail.com>
2022-11-20 16:18:44 -0700

    embed query data in test_formatter2.py

--- test_formatter2.py
+++ test_formatter2.py
@@ -11,37 +11,687 @@ def setup_module():
     sf_flags.reset_to_defaults()
 
 
-BASE_DIRECTORY = "queries_for_test"
+class QueriesForTest:
+    def get_ids(self):
+        return [name for name, queries in self.__dict__.items()]
+
+    def get_inputs(self):
+        return [queries["input"].rstrip("\n") for name, queries in self.__dict__.items()]
+
+    def get_outputs__default(self):
+        return [queries["default"].rstrip("\n") for name, queries in self.__dict__.items()]
+
+    def get_outputs__trim_leading_whitespace(self):
+        return [queries["trim_leading_whitespace"].rstrip("\n") for name, queries in self.__dict__.items()]
+
+    def get_outputs__compact_expressions(self):
+        return [queries["compact_expressions"].rstrip("\n") for name, queries in self.__dict__.items()]
 
 
-test_inputs = []
-expected_outputs = {
-    "trim_leading_whitespace_OFF__compact_expressions_OFF": [],
-    "trim_leading_whitespace_ON__compact_expressions_OFF": [],
-    "trim_leading_whitespace_OFF__compact_expressions_ON": [],
-    "trim_leading_whitespace_ON__compact_expressions_ON": [],
-}
-test_ids = []
-for input_path in Path(BASE_DIRECTORY).glob("*_IN.sql"):
-    input_contents = input_path.read_text("utf-8").rstrip("\n")
-    test_inputs.append(input_contents)
+qft = QueriesForTest()
 
-    output_name = input_path.name.replace("_IN.sql", "_OUT.sql")
-    for variation in expected_outputs.keys():
-        output_path = Path(BASE_DIRECTORY) / variation / output_name
-        output_contents = output_path.read_text("utf-8").rstrip("\n")
-        expected_outputs[variation].append(output_contents)
 
-    test_id = input_path.name.replace("_IN.sql", "")
-    test_ids.append(test_id)
+qft.cte_with_typescript_string_templating = dict(
+input="""\
+with cte1 as (select 1 from bar where 1=1)
+, cte2 as (${blergh})
+, cte3 as (select 2 from baz where 1=1)
+select foo from table1 where 1=1
+""",
+
+default="""\
+with cte1 as
+(
+    select 1
+      from bar
+     where 1=1
+)
+, cte2 as
+(
+    ${blergh}
+)
+, cte3 as
+(
+    select 2
+      from baz
+     where 1=1
+)
+select foo
+  from table1
+ where 1=1
+""",
+
+trim_leading_whitespace="""\
+with cte1 as
+(
+    select 1
+      from bar
+     where 1=1
+)
+, cte2 as
+(
+    ${blergh}
+)
+, cte3 as
+(
+    select 2
+      from baz
+     where 1=1
+)
+select foo
+  from table1
+ where 1=1
+""",
+
+compact_expressions="""\
+with cte1 as
+(
+    select 1
+      from bar
+     where 1=1
+)
+, cte2 as
+(
+    ${blergh}
+)
+, cte3 as
+(
+    select 2
+      from baz
+     where 1=1
+)
+select foo
+  from table1
+ where 1=1
+""",
+)
+
+qft.limit_offset = dict(
+input="""\
+select foo from table1 where 1=1 order by foo limit 9 offset 0
+""",
+
+default="""\
+select foo
+  from table1
+ where 1=1
+ order by foo
+ limit 9
+offset 0
+""",
+
+trim_leading_whitespace="""\
+select foo
+  from table1
+ where 1=1
+ order by foo
+ limit 9
+offset 0
+""",
+
+compact_expressions="""\
+select foo
+  from table1
+ where 1=1
+ order by foo
+ limit 9
+offset 0
+""",
+)
+
+qft.nested_inline_views = dict(
+input="""\
+select x.foo, current_date, 111
+from (
+    select y.foo, current_timestamp, 222
+    from (
+        select foo, random(), 333
+        from bar
+        where 1=1
+        and foo is not null
+    ) y
+    where 1=1 and y.foo > 0
+) x
+where 1=1 and mod(x.foo, 2) = 0
+""",
+
+default="""\
+select x.foo
+     , current_date
+     , 111
+  from (select y.foo
+             , current_timestamp
+             , 222
+          from (select foo
+                     , random()
+                     , 333
+                  from bar
+                 where 1=1
+                   and foo is not null
+               ) y
+         where 1=1
+           and y.foo > 0
+       ) x
+ where 1=1
+   and mod(x.foo, 2) = 0
+""",
+
+trim_leading_whitespace="""\
+select x.foo
+     , current_date
+     , 111
+  from (select y.foo
+             , current_timestamp
+             , 222
+          from (select foo
+                     , random()
+                     , 333
+                  from bar
+                 where 1=1
+                   and foo is not null
+               ) y
+         where 1=1
+           and y.foo > 0
+       ) x
+ where 1=1
+   and mod(x.foo, 2) = 0
+""",
+
+compact_expressions="""\
+select x.foo
+     , current_date
+     , 111
+  from (select y.foo
+             , current_timestamp
+             , 222
+          from (select foo
+                     , random()
+                     , 333
+                  from bar
+                 where 1=1
+                   and foo is not null
+               ) y
+         where 1=1
+           and y.foo > 0
+       ) x
+ where 1=1
+   and mod(x.foo, 2) = 0
+""",
+)
+
+qft.select_from_join = dict(
+input="""\
+select foo, bar, baz from table1 join table2 on(table1.id = table2.table1_id)
+""",
+
+default="""\
+select foo
+     , bar
+     , baz
+  from table1
+  join table2 on(table1.id = table2.table1_id)
+""",
+
+trim_leading_whitespace="""\
+select foo
+     , bar
+     , baz
+  from table1
+  join table2 on(table1.id = table2.table1_id)
+""",
+
+compact_expressions="""\
+select foo
+     , bar
+     , baz
+  from table1
+  join table2 on(table1.id = table2.table1_id)
+""",
+)
+
+qft.scalar_subquery = dict(
+input="""\
+select foo, (select 1 from baz where 1=1) as SCALAR_SUBQUERY from bar where 1=1
+""",
+
+default="""\
+select foo
+     , (select 1
+          from baz
+         where 1=1
+       ) as SCALAR_SUBQUERY
+  from bar
+ where 1=1
+""",
+
+trim_leading_whitespace="""\
+select foo
+     , (select 1
+          from baz
+         where 1=1
+       ) as SCALAR_SUBQUERY
+  from bar
+ where 1=1
+""",
+
+compact_expressions="""\
+select foo
+     , (select 1
+          from baz
+         where 1=1
+       ) as SCALAR_SUBQUERY
+  from bar
+ where 1=1
+""",
+)
+
+qft.trivial_cte = dict(
+input="""\
+with cte1 as (select 1 from bar where 1=1) select foo from table1 where 1=1
+""",
+
+default="""\
+with cte1 as
+(
+    select 1
+      from bar
+     where 1=1
+)
+select foo
+  from table1
+ where 1=1
+""",
+
+trim_leading_whitespace="""\
+with cte1 as
+(
+    select 1
+      from bar
+     where 1=1
+)
+select foo
+  from table1
+ where 1=1
+""",
+
+compact_expressions="""\
+with cte1 as
+(
+    select 1
+      from bar
+     where 1=1
+)
+select foo
+  from table1
+ where 1=1
+""",
+)
+
+qft.trailing_commas = dict(
+input="""\
+select
+    foo,
+    bar,
+    baz,
+    count(1)
+from
+    table1
+join
+    table2
+    on(table1.id = table2.table1_id)
+where 1=1
+    and foo > bar
+group by
+    foo,
+    bar,
+    baz
+""",
+
+default="""\
+select
+       foo
+     ,
+       bar
+     ,
+       baz
+     ,
+       count(1)
+  from
+       table1
+  join
+       table2
+       on(table1.id = table2.table1_id)
+ where 1=1
+   and foo > bar
+ group by
+          foo
+        ,
+          bar
+        ,
+          baz
+""",
+
+trim_leading_whitespace="""\
+select foo
+     , bar
+     , baz
+     , count(1)
+  from table1
+  join table2
+       on(table1.id = table2.table1_id)
+ where 1=1
+   and foo > bar
+ group by foo
+        , bar
+        , baz
+""",
+
+compact_expressions="""\
+select foo
+     , bar
+     , baz
+     , count(1)
+  from table1
+  join table2 on(table1.id = table2.table1_id)
+ where 1=1
+   and foo > bar
+ group by foo
+        , bar
+        , baz
+""",
+)
+
+qft.union_all = dict(
+input="""\
+select foo from table1 where 1=1 union all select foo from table2 where 1=1
+""",
+
+default="""\
+select foo
+  from table1
+ where 1=1
+union all
+select foo
+  from table2
+ where 1=1
+""",
+
+trim_leading_whitespace="""\
+select foo
+  from table1
+ where 1=1
+union all
+select foo
+  from table2
+ where 1=1
+""",
+
+compact_expressions="""\
+select foo
+  from table1
+ where 1=1
+union all
+select foo
+  from table2
+ where 1=1
+""",
+)
+
+qft.window_function = dict(
+input="""\
+select foo, lead(foo, 1) over(order by event_datetime) as NEXT_FOO from bar where 1=1
+""",
+
+default="""\
+select foo
+     , lead(foo, 1) over(order by event_datetime) as NEXT_FOO
+  from bar
+ where 1=1
+""",
+
+trim_leading_whitespace="""\
+select foo
+     , lead(foo, 1) over(order by event_datetime) as NEXT_FOO
+  from bar
+ where 1=1
+""",
+
+compact_expressions="""\
+select foo
+     , lead(foo, 1) over(order by event_datetime) as NEXT_FOO
+  from bar
+ where 1=1
+""",
+)
+
+qft.leading_line_comment = dict(
+input="""\
+--comment
+select foo from bar where 1=1
+""",
+
+default="""\
+--comment
+select foo
+  from bar
+ where 1=1
+""",
+
+trim_leading_whitespace="""\
+--comment
+select foo
+  from bar
+ where 1=1
+""",
+
+compact_expressions="""\
+--comment
+select foo
+  from bar
+ where 1=1
+""",
+)
+
+qft.inline_view = dict(
+input="""\
+select x.foo from (select foo from bar where 1=1) x where 1=1
+""",
+
+default="""\
+select x.foo
+  from (select foo
+          from bar
+         where 1=1
+       ) x
+ where 1=1
+""",
+
+trim_leading_whitespace="""\
+select x.foo
+  from (select foo
+          from bar
+         where 1=1
+       ) x
+ where 1=1
+""",
+
+compact_expressions="""\
+select x.foo
+  from (select foo
+          from bar
+         where 1=1
+       ) x
+ where 1=1
+""",
+)
+
+qft.keywordy_functions_in_where = dict(
+input="""\
+select 1 from bar where 1=1 and substring(foo from 2 for 3) != 'xxx' and extract('year' from event_datetime) > 2000
+""",
+
+default="""\
+select 1
+  from bar
+ where 1=1
+   and substring(foo from 2 for 3) != 'xxx'
+   and extract('year' from event_datetime) > 2000
+""",
+
+trim_leading_whitespace="""\
+select 1
+  from bar
+ where 1=1
+   and substring(foo from 2 for 3) != 'xxx'
+   and extract('year' from event_datetime) > 2000
+""",
+
+compact_expressions="""\
+select 1
+  from bar
+ where 1=1
+   and substring(foo from 2 for 3) != 'xxx'
+   and extract('year' from event_datetime) > 2000
+""",
+)
+
+qft.leading_block_comment = dict(
+input="""\
+/* comment */
+select foo from bar where 1=1
+""",
+
+default="""\
+/* comment */
+select foo
+  from bar
+ where 1=1
+""",
+
+trim_leading_whitespace="""\
+/* comment */
+select foo
+  from bar
+ where 1=1
+""",
+
+compact_expressions="""\
+/* comment */
+select foo
+  from bar
+ where 1=1
+""",
+)
+
+qft.inlist_subquery = dict(
+input="""\
+select foo, id in (select bar_id from baz where 1=1) as INLIST_SUBQUERY from bar where 1=1
+""",
+
+default="""\
+select foo
+     , id in (select bar_id
+                from baz
+               where 1=1
+             ) as INLIST_SUBQUERY
+  from bar
+ where 1=1
+""",
+
+trim_leading_whitespace="""\
+select foo
+     , id in (select bar_id
+                from baz
+               where 1=1
+             ) as INLIST_SUBQUERY
+  from bar
+ where 1=1
+""",
+
+compact_expressions="""\
+select foo
+     , id in (select bar_id
+                from baz
+               where 1=1
+             ) as INLIST_SUBQUERY
+  from bar
+ where 1=1
+""",
+)
+
+qft.subquery_in_where = dict(
+input="""\
+select foo from bar where 1=1 and id in (select bar_id from bar_baz_map where 1=1)
+""",
+
+default="""\
+select foo
+  from bar
+ where 1=1
+   and id in (select bar_id
+                from bar_baz_map
+               where 1=1
+             )
+""",
+
+trim_leading_whitespace="""\
+select foo
+  from bar
+ where 1=1
+   and id in (select bar_id
+                from bar_baz_map
+               where 1=1
+             )
+""",
+
+compact_expressions="""\
+select foo
+  from bar
+ where 1=1
+   and id in (select bar_id
+                from bar_baz_map
+               where 1=1
+             )
+""",
+)
+
+qft.simple_grouped_count = dict(
+input="""\
+select foo, count(1) from bar where 1=1 group by foo
+""",
+
+default="""\
+select foo
+     , count(1)
+  from bar
+ where 1=1
+ group by foo
+""",
+
+trim_leading_whitespace="""\
+select foo
+     , count(1)
+  from bar
+ where 1=1
+ group by foo
+""",
+
+compact_expressions="""\
+select foo
+     , count(1)
+  from bar
+ where 1=1
+ group by foo
+""",
+)
 
 
 @pytest.mark.parametrize(
     "test_input,expected_output",
-    zip(test_inputs, expected_outputs["trim_leading_whitespace_OFF__compact_expressions_OFF"]),
-    ids=test_ids
+    zip(qft.get_inputs(), qft.get_outputs__default()),
+    ids=qft.get_ids()
 )
-def test_do_format__trim_leading_whitespace_OFF__compact_expressions_OFF(test_input, expected_output):
+def test_do_format__default(test_input, expected_output):
     sf_flags.TRIM_LEADING_WHITESPACE = False
     sf_flags.COMPACT_EXPRESSIONS = False
     actual_output = do_format(test_input)
@@ -52,10 +702,10 @@ def test_do_format__trim_leading_whitespace_OFF__compact_expressions_OFF(test_in
 
 @pytest.mark.parametrize(
     "test_input,expected_output",
-    zip(test_inputs, expected_outputs["trim_leading_whitespace_ON__compact_expressions_OFF"]),
-    ids=test_ids
+    zip(qft.get_inputs(), qft.get_outputs__trim_leading_whitespace()),
+    ids=qft.get_ids()
 )
-def test_do_format__trim_leading_whitespace_ON__compact_expressions_OFF(test_input, expected_output):
+def test_do_format__trim_leading_whitespace(test_input, expected_output):
     sf_flags.TRIM_LEADING_WHITESPACE = True
     sf_flags.COMPACT_EXPRESSIONS = False
     actual_output = do_format(test_input)
@@ -66,24 +716,10 @@ def test_do_format__trim_leading_whitespace_ON__compact_expressions_OFF(test_inp
 
 @pytest.mark.parametrize(
     "test_input,expected_output",
-    zip(test_inputs, expected_outputs["trim_leading_whitespace_OFF__compact_expressions_ON"]),
-    ids=test_ids
+    zip(qft.get_inputs(), qft.get_outputs__compact_expressions()),
+    ids=qft.get_ids()
 )
-def test_do_format__trim_leading_whitespace_OFF__compact_expressions_ON(test_input, expected_output):
-    sf_flags.TRIM_LEADING_WHITESPACE = False
-    sf_flags.COMPACT_EXPRESSIONS = True
-    actual_output = do_format(test_input)
-    print(actual_output)
-    #print(actual_output.replace(" ", "⦁"))
-    assert expected_output == actual_output
-
-
-@pytest.mark.parametrize(
-    "test_input,expected_output",
-    zip(test_inputs, expected_outputs["trim_leading_whitespace_ON__compact_expressions_ON"]),
-    ids=test_ids
-)
-def test_do_format__trim_leading_whitespace_ON__compact_expressions_ON(test_input, expected_output):
+def test_do_format__compact_expressions(test_input, expected_output):
     sf_flags.TRIM_LEADING_WHITESPACE = True
     sf_flags.COMPACT_EXPRESSIONS = True
     actual_output = do_format(test_input)
