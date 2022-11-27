456ba997bb1d302b37fdd9fee660dc3b8b5a2cf4
Noah Yetter <nyetter@gmail.com>
2022-11-27 15:08:20 -0700

    first steps: swap out flags for enum in sf_flags

--- sf_flags.py
+++ sf_flags.py
@@ -1,15 +1,19 @@
+import enum
 # The "constants" defined in this module will be mutated by:
 # 1. the top-level "main", after parsing command-line args
 # 2. unit tests
 # During any given execution they will be set, and then never change.
 
-TRIM_LEADING_WHITESPACE = False
 
-COMPACT_EXPRESSIONS = False
+class FormatMode(enum.Enum):
+    DEFAULT = "DEFAULT"
+    TRIM_LEADING_WHITESPACE = "TRIM_LEADING_WHITESPACE"
+    COMPACT_EXPRESSIONS = "COMPACT_EXPRESSIONS"
+
+
+FORMAT_MODE = FormatMode.DEFAULT
+
 
 def reset_to_defaults():
-    global TRIM_LEADING_WHITESPACE
-    TRIM_LEADING_WHITESPACE = False
-
-    global COMPACT_EXPRESSIONS
-    COMPACT_EXPRESSIONS = False
+    global FORMAT_MODE
+    FORMAT_MODE = FormatMode.DEFAULT
