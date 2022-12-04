1d41c710310a289751d7d837d69c2d1d99a7a19a
Noah Yetter <nyetter@gmail.com>
2022-12-04 13:46:58 -0700

    very beginnings of a custom lexer

---
+++ test_sflexer.py
@@ -0,0 +1,146 @@
+import pytest
+
+import sflexer
+from sftoken import SFToken, SFTokenKind
+
+
+def test_integer_literal():
+    text = "1"
+    expected = SFToken(SFTokenKind.WORD, "1")
+    actual = sflexer.lex(text)
+    assert expected == actual
+
+
+def test_decimal_literal():
+    text = "3.14"
+    expected = SFToken(SFTokenKind.WORD, "3.14")
+    actual = sflexer.lex(text)
+    assert expected == actual
+
+
+def test_scientific_literal():
+    text = "1.23e4"
+    expected = SFToken(SFTokenKind.WORD, "1.23e4")
+    actual = sflexer.lex(text)
+    assert expected == actual
+
+
+def test_word():
+    text = "foo"
+    expected = SFToken(SFTokenKind.WORD, "foo")
+    actual = sflexer.lex(text)
+    assert expected == actual
+
+
+def test_words_with_spaces():
+    text = "foo bar baz"
+    expected = [
+        SFToken(SFTokenKind.WORD, "foo"),
+        SFToken(SFTokenKind.SPACES, " "),
+        SFToken(SFTokenKind.WORD, "bar"),
+        SFToken(SFTokenKind.SPACES, " "),
+        SFToken(SFTokenKind.WORD, "baz"),
+    ]
+    actual = sflexer.lex(text)
+    assert expected == actual
+
+
+def test_words_with_newlines():
+    text = "foo\nbar\nbaz"
+    expected = [
+        SFToken(SFTokenKind.WORD, "foo"),
+        SFToken(SFTokenKind.NEWLINE, "\n"),
+        SFToken(SFTokenKind.WORD, "bar"),
+        SFToken(SFTokenKind.NEWLINE, "\n"),
+        SFToken(SFTokenKind.WORD, "baz"),
+    ]
+    actual = sflexer.lex(text)
+    assert expected == actual
+
+
+def test_single_quoted_literal():
+    text = "'literal'"
+    expected = SFToken(SFTokenKind.LITERAL, "'literal'")
+    actual = sflexer.lex(text)
+    assert expected == actual
+
+
+def test_double_quoted_identifier():
+    text = '"identifier"'
+    expected = SFToken(SFTokenKind.LITERAL, '"identifier"')
+    actual = sflexer.lex(text)
+    assert expected == actual
+
+
+def test_backtick_quoted_identifier():
+    text = "`identifier`"
+    expected = SFToken(SFTokenKind.LITERAL, "`identifier`")
+    actual = sflexer.lex(text)
+    assert expected == actual
+
+
+def test_dotted_identifier()
+    text = "foo.bar"
+    expected = SFToken(SFTokenKind.WORD, "foo.bar")
+    actual = sflexer.lex(text)
+    assert expected == actual
+
+
+def test_line_comment(): # this one case is probably not sufficient
+    text = "foo\n--hey\nbar"
+    expected = [
+        SFToken(SFTokenKind.WORD, "foo"),
+        SFToken(SFTokenKind.NEWLINE, "\n"),
+        SFToken(SFTokenKind.LINE_COMMENT, "--hey\n"),
+        SFToken(SFTokenKind.WORD, "bar"),
+    ]
+    actual = sflexer.lex(text)
+    assert expected == actual
+
+
+def test_block_comment(): # this one case is probably not sufficient
+    text = "foo/* hey */bar"
+    expected = [
+        SFToken(SFTokenKind.WORD, "foo"),
+        SFToken(SFTokenKind.BLOCK_COMMENT, "/* hey */"),
+        SFToken(SFTokenKind.WORD, "bar"),
+    ]
+    actual = sflexer.lex(text)
+    assert expected == actual
+
+
+def test_symbols():
+    symbols = [
+        "~",
+        "!",
+        "@",
+        "#",
+        "$",
+        "%",
+        "^",
+        "&",
+        "*",
+        "(",
+        ")",
+        "-",
+        "+",
+        "=",
+        "[",
+        "]",
+        "{",
+        "}",
+        "|",
+        ",",
+        "<",
+        ">",
+        "?",
+        "/",
+        ":",
+        ";",
+    ]
+
+    for s in symbol:
+        tokens = sflexer.lex(s)
+        expected = SFToken(SFTokenKind.SYMBOL, s)
+        actual = tokens[0]
+        assert expected == actual
