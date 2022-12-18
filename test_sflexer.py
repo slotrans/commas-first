012b625975558a9c14a6f5142c0168aeabe98b82
Noah Yetter <nyetter@gmail.com>
2022-12-18 16:34:24 -0700

    a stab at it, some passing tests, many failing, an infinite loop in there somewhere

--- test_sflexer.py
+++ test_sflexer.py
@@ -4,30 +4,59 @@ import sflexer
 from sftoken import SFToken, SFTokenKind
 
 
+def test_none():
+    text = None
+    expected = []
+    actual = sflexer.lex(text)
+    assert expected == actual
+
+
+def test_empty():
+    text = ""
+    expected = []
+    actual = sflexer.lex(text)
+    assert expected == actual
+
+
+def test_newline():
+    text = "\n"
+    expected = [SFToken(SFTokenKind.NEWLINE, "\n")]
+    actual = sflexer.lex(text)
+    assert expected == actual
+
+
+def test_spaces():
+    for i in range(1, 10): # arbitrary
+        i_spaces = " " * i
+        expected = [SFToken(SFTokenKind.SPACES, i_spaces)]
+        actual = sflexer.lex(i_spaces)
+        assert expected == actual
+
+
 def test_integer_literal():
     text = "1"
-    expected = SFToken(SFTokenKind.WORD, "1")
+    expected = [SFToken(SFTokenKind.WORD, "1")]
     actual = sflexer.lex(text)
     assert expected == actual
 
 
 def test_decimal_literal():
     text = "3.14"
-    expected = SFToken(SFTokenKind.WORD, "3.14")
+    expected = [SFToken(SFTokenKind.WORD, "3.14")]
     actual = sflexer.lex(text)
     assert expected == actual
 
 
 def test_scientific_literal():
     text = "1.23e4"
-    expected = SFToken(SFTokenKind.WORD, "1.23e4")
+    expected = [SFToken(SFTokenKind.WORD, "1.23e4")]
     actual = sflexer.lex(text)
     assert expected == actual
 
 
 def test_word():
     text = "foo"
-    expected = SFToken(SFTokenKind.WORD, "foo")
+    expected = [SFToken(SFTokenKind.WORD, "foo")]
     actual = sflexer.lex(text)
     assert expected == actual
 
@@ -60,28 +89,28 @@ def test_words_with_newlines():
 
 def test_single_quoted_literal():
     text = "'literal'"
-    expected = SFToken(SFTokenKind.LITERAL, "'literal'")
+    expected = [SFToken(SFTokenKind.LITERAL, "'literal'")]
     actual = sflexer.lex(text)
     assert expected == actual
 
 
 def test_double_quoted_identifier():
     text = '"identifier"'
-    expected = SFToken(SFTokenKind.LITERAL, '"identifier"')
+    expected = [SFToken(SFTokenKind.LITERAL, '"identifier"')]
     actual = sflexer.lex(text)
     assert expected == actual
 
 
 def test_backtick_quoted_identifier():
     text = "`identifier`"
-    expected = SFToken(SFTokenKind.LITERAL, "`identifier`")
+    expected = [SFToken(SFTokenKind.LITERAL, "`identifier`")]
     actual = sflexer.lex(text)
     assert expected == actual
 
 
-def test_dotted_identifier()
+def test_dotted_identifier():
     text = "foo.bar"
-    expected = SFToken(SFTokenKind.WORD, "foo.bar")
+    expected = [SFToken(SFTokenKind.WORD, "foo.bar")]
     actual = sflexer.lex(text)
     assert expected == actual
 
@@ -139,8 +168,7 @@ def test_symbols():
         ";",
     ]
 
-    for s in symbol:
-        tokens = sflexer.lex(s)
-        expected = SFToken(SFTokenKind.SYMBOL, s)
-        actual = tokens[0]
+    for s in symbols:
+        expected = [SFToken(SFTokenKind.SYMBOL, s)]
+        actual = sflexer.lex(s)
         assert expected == actual
